16c8dc6cc9e46ddd200cfe18c94e4ab1738070de
mragils <mragilsahyuda@gmail.com>
2023-12-12 16:34:13 +0700

    update tic tac toe js

--- tictactoe.js
+++ tictactoe.js
@@ -4,13 +4,20 @@ window.addEventListener("DOMContentLoaded", () => {
   const resetButton = document.querySelector("#reset");
   const announcer = document.querySelector(".announcer");
 
-  let board = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+  let board = ["", "", "", "", "", "", "", "", ""];
   let currentPlayer = "X";
   let isGameActive = true;
 
   const PLAYERX_WON = "PLAYERX_WON";
   const PLAYERO_WON = "PLAYERO_WON";
   const TIE = "TIE";
+
+  /*
+      Indexes within the board
+      [0] [1] [2]
+      [3] [4] [5]
+      [6] [7] [8]
+  */
 
   const winningConditions = [
     [0, 1, 2],
@@ -25,17 +32,15 @@ window.addEventListener("DOMContentLoaded", () => {
 
   function handleResultValidation() {
     let roundWon = false;
-    for (let i = 0; i < winningConditions.length; i++) {
+    for (let i = 0; i <= 7; i++) {
       const winCondition = winningConditions[i];
       const a = board[winCondition[0]];
       const b = board[winCondition[1]];
       const c = board[winCondition[2]];
-
       if (a === "" || b === "" || c === "") {
         continue;
       }
-
-      if (a === b && b === c && a === currentPlayer) {
+      if (a === b && b === c) {
         roundWon = true;
         break;
       }
@@ -47,11 +52,7 @@ window.addEventListener("DOMContentLoaded", () => {
       return;
     }
 
-    if (!board.includes(" ")) {
-      announce(TIE);
-      isGameActive = false;
-      return;
-    }
+    if (!board.includes("")) announce(TIE);
   }
 
   const announce = (type) => {
